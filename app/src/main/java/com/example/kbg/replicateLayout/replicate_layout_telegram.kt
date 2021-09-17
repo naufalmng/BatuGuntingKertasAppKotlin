78737f13555272d0289d7b042dee1ecd680f9ccf
naufalmng <naufalmng@gmail.com>
2021-09-17 15:57:36 +0700

    fourth commit

---
+++ app/src/main/java/com/example/kbg/replicateLayout/replicate_layout_telegram.kt
@@ -0,0 +1,11 @@
+package com.example.kbg.replicateLayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class replicate_layout_telegram : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_replicate_layout_telegram)
+    }
+}
